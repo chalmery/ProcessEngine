1fd4278c9bf52a889aa78bc8bd0186ed910dc06a
四分之一个全栈 <95135709+chalmery@users.noreply.github.com>
2023-11-26 11:17:24 +0800

    :art: init

---
+++ src/main/java/top/yangcc/processengine/ProcessEngineApplication.java
@@ -0,0 +1,13 @@
+package top.yangcc.processengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessEngineApplication.class, args);
+    }
+
+}
